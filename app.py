4c9f552711ef2033ec76b3b4a06ae814892a293e
weimegn@126.com <weimegn@126.com>
2019-04-13 08:56:49 +0800

    update

--- app.py
+++ app.py
@@ -11,7 +11,12 @@
 
 from flask import Flask
 
-app = Flask('__name__')
+
+app = Flask(__name__)
+app.config.update({
+    "DEBUG": True,
+    "TEMPLATES_AUTO_RELOAD": True
+})
 
 
 if __name__=='__main__':
