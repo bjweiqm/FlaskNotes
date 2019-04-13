f51021a4cb3cd431561fbeee01adaec96d76c48b
weimegn@126.com <weimegn@126.com>
2019-04-13 08:46:49 +0800

    update

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   app.py
+@Time    :   2019/04/13 08:46:32
+@Author  :   WM 
+@Version :   1.0
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
+
+
+if __name__=='__main__':
+   pass
