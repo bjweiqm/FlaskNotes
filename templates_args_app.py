4b183e70e520eff8abc459489cbed35e98fa6d08
weimegn@126.com <weimegn@126.com>
2019-04-19 19:51:55 +0800

    update

---
+++ templates_args_app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+
+    return render_template()
+
+
+if __name__ == '__main__':
+    app.run()
