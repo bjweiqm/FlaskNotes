d7f665e7ac4077ced7e5288fe87e0d2e5657dda1
weimegn@126.com <weimegn@126.com>
2019-04-19 17:08:55 +0800

    update

--- url_response_app.py
+++ url_response_app.py
@@ -15,6 +15,15 @@ app.debug = True
 # 将视图函数中返回字典，转换成json对象然后返回
 # restful-api
 
+
+class JsonReponse(Response):
+    @classmethod
+    def force_type(cls, response, environ=None):
+        '''
+        这个方法只有视图函数返回非字符串、元组、Response对象，才会调用。
+        '''
+        print(response)
+
 @app.route('/')
 def index():
 
