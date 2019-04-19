4a312d82abd4606d0bdcf868d11a7f0f45ee46ec
weimegn@126.com <weimegn@126.com>
2019-04-19 17:55:55 +0800

    update

--- template_app.py
+++ template_app.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 
-    return 'Hello World !'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
