fc0828203eafa455698fc06b9527b21fa3900e06
weimegn@126.com <weimegn@126.com>
2019-04-25 18:39:57 +0800

    update

--- JSTestDemo/js/demo1.js
+++ JSTestDemo/js/demo1.js
@@ -10,22 +10,24 @@ console.log('zhgs\nlsjfl\nlsjflsj')
 var name = "小明";
 var age = 19;
 var message = '你好' + name + '你的年龄' + age + '岁了！';
-console.log(message)
+console.log(message);
 
-var len = "Hello, world!"
-console.log(len.length)
+console.log('你好 你今年已经 岁了，该上学了小伙，哈哈哈哈哈');
 
-console.log(len[5])
+// var len = "Hello, world!"
+// console.log(len.length)
 
-if (2 > 1) {
-    console.log('True')
-} else {
-    console.log('False')
-}
+// console.log(len[5])
 
-if (1 == '1') {
-    console.log('True' + '与上面区分')
-} else {
-    console.log('False' + '与上面区分')
+// if (2 > 1) {
+//     console.log('True')
+// } else {
+//     console.log('False')
+// }
+
+// if (1 == '1') {
+//     console.log('True' + '与上面区分')
+// } else {
+//     console.log('False' + '与上面区分')
 }
 
