5e76dfa9641863b25b289f5521a3e274237cc4b3
weimegn@126.com <weimegn@126.com>
2019-04-26 10:07:27 +0800

    update

--- JSTestDemo/js/demo1.js
+++ JSTestDemo/js/demo1.js
@@ -1,3 +1,12 @@
+
+function test(x){
+    console.log('x = ' + x)
+    for(i=0; i < arguments.length; i++){
+        
+    }
+}
+
+
 // alert('外部引入文件。');
 /*
 var x = 100;
@@ -113,18 +122,18 @@ for (i = 1; i < 11; i ++){
 
 
 
-var a = ['A', 'B', 'C'];
-var s = new Set(['A', 'B', 'C']);
-var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
-for (var x of a) { // 遍历Array
-    console.log(x);
-}
-for (var x of s) { // 遍历Set
-    console.log(x);
-}
-for (var x of m) { // 遍历Map
-    console.log(x[0] + '=' + x[1]);
-}
+// var a = ['A', 'B', 'C'];
+// var s = new Set(['A', 'B', 'C']);
+// var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
+// for (var x of a) { // 遍历Array
+//     console.log(x);
+// }
+// for (var x of s) { // 遍历Set
+//     console.log(x);
+// }
+// for (var x of m) { // 遍历Map
+//     console.log(x[0] + '=' + x[1]);
+// }
 
 
 
